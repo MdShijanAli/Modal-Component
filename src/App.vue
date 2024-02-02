d707e21d2b110ebfcb2f981d9dbb1f0939b4b8c4
MdShijanAli <freelancershijan@gmail.com>
2024-02-02 23:34:52 +0600

    Fists Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Karla', sans-serif;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <ModalComponent @close="toggleModal" :modalActive="modalActive">
+      <div class="modal-content">
+         <h1>This is a Modal Header</h1>
+         <p>This is Modal Messag4</p>
+      </div>
+     </ModalComponent>
+     <button @click="toggleModal">Open Modal</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ModalComponent from '@/components/ModalComponent.vue';
+import { ref } from 'vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ModalComponent,
+  },
+
+  setup(){
+    const modalActive = ref(false);
+
+    const toggleModal = () =>{
+      modalActive.value = !modalActive.value
+    }
+
+    return {modalActive,toggleModal}
   }
 }
 </script>
+
+
+<style scoped>
+.home{
+  background-color: rgba(0,176, 234, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-content{
+  display: flex;
+  flex-direction: column;
+}
+.modal-content h1,
+.modal-content p{
+  margin-bottom: 16px;
+}
+.modal-content h1{
+  font-size: 32px;
+}
+</style>
